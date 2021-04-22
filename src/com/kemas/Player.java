89c020dda817e8c612f269f51121cd18b0018e57
Kemas Raihan <b6024493@my.shu.ac.uk>
2021-04-22 04:54:27 +0100

    Created Java class to record player score

---
+++ src/com/kemas/Player.java
@@ -0,0 +1,11 @@
+package com.kemas;
+
+public class Player {
+    private String name;
+    private int points;
+
+    public Player(String newName, int newPoints){
+        name=newName;
+        points=newPoints;
+    }
+}
