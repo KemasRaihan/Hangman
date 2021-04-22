0ea3c0d293061e69d64da25c6c0ea0e45457c9c0
Kemas Raihan <b6024493@my.shu.ac.uk>
2021-04-22 05:03:04 +0100

    Created method 'welcomePlayer()'

--- src/com/kemas/Main.java
+++ src/com/kemas/Main.java
@@ -1,9 +1,18 @@
 package com.kemas;
+import java.util.Scanner;
 
 public class Main {
 
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
-        //
+        welcomePlayer();
     }
+
+    public static void welcomePlayer(){
+        System.out.println("Welcome to Hangman!");
+        System.out.println("Press 'Enter' to play.");
+        input.nextLine();
+    }
+
 }
