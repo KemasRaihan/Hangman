ae32a114d8bc0b678df620acb229b6f1dfcef79d
Kemas Raihan <b6024493@my.shu.ac.uk>
2021-04-23 01:09:44 +0100

    Created method printHiddenCharacters().

--- src/com/kemas/Main.java
+++ src/com/kemas/Main.java
@@ -1,4 +1,6 @@
 package com.kemas;
+import org.junit.Test;
+
 import java.util.Scanner;
 
 public class Main {
@@ -19,8 +21,24 @@ public class Main {
     public static void playHangman(){
         System.out.println("You are now playing Hangman...");
 
-        String secretWord = "Hello";
+        String secretWord = "Hello there";
 
+        char[] secretWordCharacterArray = convertStringIntoCharactersArray(secretWord);
+
+        System.out.println("Guess a letter...");
+        printHiddenCharacters(secretWordCharacterArray);
+
+    }
+
+    public static char[] convertStringIntoCharactersArray(String word){
+        return word.toCharArray();
+    }
+
+    public static void printHiddenCharacters(char[] charArray){
+        for(int i = 0; i < charArray.length; i++){
+            if(charArray[i]==' ') System.out.print(" ");
+            else System.out.print("_");
+        }
     }
 
 
